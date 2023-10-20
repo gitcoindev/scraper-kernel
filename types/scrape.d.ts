10fd79fdfe9fe67c5555ec1af06c5efac55a05ea
Korrrba <gitcoiner@gmail.com>
2023-10-20 18:11:34 +0200

    chore: declare module global in scrape.d.ts

--- types/scrape.d.ts
+++ types/scrape.d.ts
@@ -1,11 +1,12 @@
-declare module "@korrrba/scraper-kernel-fork";
-import { Browser } from "puppeteer";
-export declare const events: any;
-export type JobResult = Error | string | null;
-export interface UserSettings {
-  urls: string[] | string;
-  pages: string;
-  chromium?: string[];
-  headful?: boolean;
+declare module "@korrrba/scraper-kernel-fork" {
+  import { Browser } from "puppeteer";
+  export declare const events: any;
+  export type JobResult = Error | string | null;
+  export interface UserSettings {
+    urls: string[] | string;
+    pages: string;
+    chromium?: string[];
+    headful?: boolean;
+  }
+  export default function scrape(settings: UserSettings, browser?: Browser): Promise<JobResult | JobResult[]>;
 }
-export default function scrape(settings: UserSettings, browser?: Browser): Promise<JobResult | JobResult[]>;
